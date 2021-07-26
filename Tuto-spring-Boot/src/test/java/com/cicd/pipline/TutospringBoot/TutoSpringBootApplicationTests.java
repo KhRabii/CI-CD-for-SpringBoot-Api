284ba93d946a2e9fd39afe896fa1aa79304e8a9a
KhRabii <42442111+KhRabii@users.noreply.github.com>
2021-07-27 00:07:16 +0100

    First-Comit

---
+++ Tuto-spring-Boot/src/test/java/com/cicd/pipline/TutospringBoot/TutoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.pipline.TutospringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
